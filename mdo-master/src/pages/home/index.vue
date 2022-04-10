ad4414a8e5c87753f110e3d589195a0078e569bb
xxj435 <861479614@qq.com>
2022-04-11 00:48:55 +0800

    首页加弹框

--- mdo-master/src/pages/home/index.vue
+++ mdo-master/src/pages/home/index.vue
@@ -2,6 +2,29 @@
   <div class="main">
     <!-- 头部 -->
     <!-- <v-header></v-header> -->
+    <div
+      class="hidden"
+      v-if="isshow"
+    >
+      <div class="hidden1">
+        <div>
+          <router-link to="/presale">
+            <img
+              src="@/assets/images/600.png"
+              alt=""
+            >
+          </router-link>
+
+          <img
+            class="close"
+            src="@/assets/images/close.png"
+            alt=""
+            @click="delimg"
+          >
+        </div>
+
+      </div>
+    </div>
     <section1></section1>
     <section2></section2>
     <section3></section3>
@@ -29,28 +52,68 @@
 </template>
 
 <script>
-import section1 from '../components/section1.vue'
-import section2 from '../components/section2.vue'
-import section3 from '../components/section3.vue'
-import header from '../components/header.vue'
-import footer from '../components/footer.vue'
+import section1 from "../components/section1.vue";
+import section2 from "../components/section2.vue";
+import section3 from "../components/section3.vue";
+import header from "../components/header.vue";
+import footer from "../components/footer.vue";
 export default {
-  name: 'home',
+  name: "home",
   components: {
     section1,
     section2,
     section3,
-    'v-header': header,
-    'v-footer': footer,
+    "v-header": header,
+    "v-footer": footer,
   },
   data() {
-    return {}
+    return {
+      isshow: true,
+    };
   },
-}
+  beforeCreate() {
+    document.querySelector("body").setAttribute("style", "overflow: hidden;");
+  },
+  methods: {
+    delimg() {
+      document.querySelector("body").removeAttribute("style");
+      this.isshow = false;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
 .main {
   scroll-behavior: smooth;
+  position: relative;
+  left: 0;
+
+  top: 0;
+  .hidden {
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    z-index: 999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .hidden1 {
+      width: 400px;
+      height: 400px;
+      position: relative;
+      .close {
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
