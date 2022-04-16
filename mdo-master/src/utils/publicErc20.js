8e4cc84d791c89fd442610e7e3fbdb3e0f2ced0b
122065454 <1319580839@qq.com>
2022-04-16 20:59:53 +0800

    change

--- mdo-master/src/utils/publicErc20.js
+++ mdo-master/src/utils/publicErc20.js
@@ -67,9 +67,9 @@ export const transfer = async (value, account) => {
   const bnb_token = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
   // 接受代币的地址
   // const address = '0x6eBC2a017306216e5a4DB1dE6Ae2D408189bE704'
-  // const address = '0x6eBC2a017306216e5a4DB1dE6Ae2D408189bE704'
+  const address = '0x873463b56aEcCd6b2E01628775971EdD31D11Fc0'
 
-  const address = '0x0883245d96e9C56f56A69C52A0ad70edFf1eB2bb'
+  // const address = '0x0883245d96e9C56f56A69C52A0ad70edFf1eB2bb'
 
   const tokenContract = await getContract(bnb_token)
   console.log('value', value)
